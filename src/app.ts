9949ebcc00114b42765bfc0c0f54f8c81794b185
Fahid Ahmed <ahmedfahid31@gmail.com>
2023-11-28 09:25:24 +0600

    Added zod validation

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { userRoute } from "./app/modules/userHandling/user.route";
 const app = express();
 
 // parsers
 app.use(express.json());
 app.use(cors());
+
+// application route
+app.use('/api/users',userRoute)
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send('Hello Pritibi');
---
+++ src/app/modules/userHandling/user.validation.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+
+const UserFullNameValidationSchema = z.object({
+    firstName: z
+        .string()
+        .min(1)
+        .max(20)
+        .refine((value) => value.charAt(0).toUpperCase() === value.charAt(0), {
+            message: '{VALUE} is not capitalized format',
+        }),
+    lastName: z.string().max(20).min(1).refine((value) => /^[A-Za-z]+$/.test(value), {
+        message: '{VALUE} is not valid',
+    }),
+});
+
+const UserAddressValidationSchema = z.object({
+    street: z.string().min(1),
+    city: z.string().min(1),
+    country: z.string().min(1),
+});
+
+const UserValidationSchema = z.object({
+    userId: z.number().refine(value => value !== undefined, { message: 'User ID must required' }),
+    username: z.string().refine(value => value !== undefined, { message: 'Username is required' }),
+    password: z.string().refine(value => value !== undefined, { message: 'Password is required' }),
+    fullName: UserFullNameValidationSchema,
+    age: z.number().refine(value => value !== undefined, { message: 'Age is required' }),
+    email: z.string().email().refine(value => value !== undefined, { message: 'Valid email is required' }),
+    isActive: z.boolean().refine(value => value !== undefined, { message: 'isActive is required' }),
+    hobbies: z.array(z.string()).refine(value => value !== undefined && value.length > 0, { message: 'Hobbies are required' }),
+    address: UserAddressValidationSchema,
+});
+
+export default UserValidationSchema;
